620435fa8ded11beec9f30e167a3d7bec1f08ef4
dpmango <xs290@me.com>
2017-07-07 15:22:25 +0300

    init

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import sayHello from './lib/sayHello.js';
+
+sayHello();
+
+$(document).on('click','.tab-control', function () {
+  var dataTab = $(this).data('tab');
+  $(this).addClass('active').siblings().removeClass('active');
+  $('.tab[data-tab="'+dataTab+'"]').addClass('active').siblings().removeClass('active');
+});
